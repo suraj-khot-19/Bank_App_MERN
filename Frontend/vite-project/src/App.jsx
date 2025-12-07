d8b5b53cf1127df3a74bb730ae0930526c65e8ce
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-12-07 09:29:59 +0530

    initial setup

---
+++ Frontend/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from './pages/login.jsx'
+import SignUp from './pages/SignUp.jsx'
+import
+  function App() {
+
+    return (
+      <>
+        <Login />
+        <SignUp />
+      </>
+    )
+  }
+
+export default App
